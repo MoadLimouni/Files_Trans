bceb2329662656623be830bb8c935476c006830c
MoadLimouni <moad.limouni@capgemini.com>
2025-03-07 11:41:46 +0000

    Small Changes

--- sender.py
+++ sender.py
@@ -1,25 +1,43 @@
 import os
 import socket
-import time  
+import time
+import hashlib
 from Crypto.Cipher import AES
-from tqdm import tqdm  # Import tqdm for progress bar
+from tqdm import tqdm  
 
 # Create and connect client socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(("localhost", 9000))
 
-# Key and nonce for encryption
-key = b"L!M&oun!M0@d8745"  
-nonce = b"M0@d8745L!M&oun!"  
+# Define file details
+key_file_path = "./key.txt"
+nonce_file_path = "./nonce.txt"
+
+# Read the key and nonce files
+with open(key_file_path, "rb") as f:
+    key = f.read()
+with open(nonce_file_path, "rb") as f:
+    nonce = f.read()
+
+# Create a cipher using the key and nonce
 cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
 
 # Define file details
 file_path = "Rapport_1_FOTA 2.pdf"
 file_size = os.path.getsize(file_path)
-file_name_tos_send = "recieved_file.pdf"
+file_name_to_send = os.path.basename(file_path)
+
+# Compute file hash
+with open(file_path, "rb") as f:
+    original_hash = hashlib.sha256(f.read()).hexdigest()
+
+# Encrypt and send the file hash
+encrypted_hash = cipher.encrypt(original_hash.encode())
+client.send(encrypted_hash)
+time.sleep(0.1)
 
 # Encrypt file name and send it
-encrypted_file_name = cipher.encrypt(file_name_tos_send.encode())
+encrypted_file_name = cipher.encrypt(file_name_to_send.encode())
 client.send(encrypted_file_name)
 time.sleep(0.1)
 
@@ -36,7 +54,7 @@ with open(file_path, "rb") as file, tqdm(total=file_size, unit="B", unit_scale=T
             break
         encrypted_data = cipher.encrypt(data)
         client.send(encrypted_data)
-        pbar.update(len(data))  
+        pbar.update(len(data))
 
 # Send encrypted end signal
 encrypted_end = cipher.encrypt(b"<END>")
